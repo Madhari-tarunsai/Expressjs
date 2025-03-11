971144f9b431a79cf7e7db14a2d2a79357b19f0a
Your Name <you@example.com>
2025-03-11 10:11:56 +0530

    added1

--- express.js
+++ express.js
@@ -1,5 +1,14 @@
-const express=require("express")
-const app=express()
-app.listen("1234",()=>{
-    console.log("server running")
-})
+const express = require("express");
+const app = express();
+app.get("/one", (req, res) => {
+  res.send("hi im from get data");
+  app.post("/one", (req, res) => {
+    res.send("hi im from post data");
+  });
+  app.patch("/one", (req, res) => {
+    res.send("hi im from patch data");
+  });
+});
+app.listen("1234", () => {
+  console.log("server running");
+});
